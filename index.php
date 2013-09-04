7244413addc78fee03f83ff2fd3bd0126192abb8
Ian <ian@Ian-Coatess-iMac.local>
2013-09-04 19:28:28 +0100

    Initial set up page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+	<title>Setting off</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+
+?>
+<p>Sign up, login, share and save the link to your smart phone.</p>
+
+
+
+</body>
+</html>
