038d51b830ee0c9d742b8ec9f80715931fe192b2
Ian <ian.coates@gmail.com>
2013-09-05 06:52:49 +0100

    update build

--- index.php
+++ index.php
@@ -6,12 +6,36 @@
 </head>
 <body>
 <?php
+/*
+Fields
+
+email
+name
+password
+id
+today_time
+today_date
+lasttime_time
+
+*/
 
 ?>
-<p>Sign up, login, share and save the link to your smart phone.</p>
+<h1>Setting off?</h1>
+<h2>Need to let a loved one know what time you've set of to meet them? You can now, with this handy app.</h2>
+<p>Sign up, login, share the link and save the page to your smart phone's home screen.</p>
 
 <?php 
 // if logged in show options, set off
+if($session()){
+        if(!isset($_GET['id']) {
+                $_GET['id'] = "";
+        }else{
+                $id = $_GET['id'];
+                //find saved information that from database that matches id.
+                $sql = "SELECT * FROM data WHERE id = '$id'";
+                
+        }
+}
 
 ?>
 
