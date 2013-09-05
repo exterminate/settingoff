819435e33abab41d75568fcf284bb1902535bacb
Ian <ian@Ian-Coatess-iMac.local>
2013-09-05 19:29:49 +0100

    updatebuild

--- index.php
+++ index.php
@@ -3,30 +3,12 @@
 <head>
 	<title>Setting off</title>
 	<link rel="stylesheet" href="style.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
 </head>
 <body>
 <?php
 
-
-/**
- * Simple example of extending the SQLite3 class and changing the __construct
- * parameters, then using the open method to initialize the DB.
- */
-class MyDB extends SQLite3
-{
-    function __construct()
-    {
-        $this->open('mysqlitedb.db');
-    }
-}
-
-$db = new MyDB();
-
-$db->exec('CREATE TABLE foo (bar STRING)');
-$db->exec("INSERT INTO foo (bar) VALUES ('This is a test')");
-
-$result = $db->query('SELECT bar FROM foo');
-var_dump($result->fetchArray());
+include "postlandr.php";
 
 //maybe look at this http://www.tutorialspoint.com/sqlite/sqlite_php.htm
 
@@ -50,12 +32,31 @@ lasttime_time
 
 <?php 
 // if logged in show options, set off
-if($session()){
-        if(!isset($_GET['id']) {
+//if($session){
+        if(!isset($_GET['id'])) {
                 $_GET['id'] = "";
                 
                 // have you forgot your unique id? Log in
-                
+?>
+<div class='register-form'>				
+	<form action="postlandr.php" method="POST">
+		<label>Name:</label><br><input type="text" name="name"><br>
+		<label>E-mail:</label><br><input type="email" name="email"><br>
+		<label>Enter Password:</label><br><input type="password" name="passone"><br>
+		<label>Enter password again:</label><br><input type="password" name="passtwo"><br>
+		<input type="submit" name="register" value="Register">
+	</form>
+	<button id="toggle-register">Login</button>
+</div>
+<div class='login-form'>				
+	<form action="postlandr.php" method="POST">		
+		<label>E-mail:</label><br><input type="email" name="email"><br>
+		<label>Enter Password:</label><br><input type="password" name="passone"><br>
+		<input type="submit" name="login" value="Login">
+	</form>
+	<button id="toggle-login">Register</button>
+</div>
+<?php                
                 // need to register?
                 
         }else{
@@ -69,8 +70,8 @@ if($session()){
                 // form
       
                 
-        }
-}
+        
+//}
 
 ?>
 
@@ -78,10 +79,25 @@ if($session()){
 <button id="set-off">Set off</button>
 
 <?php
+		}
 
 // display partners details here
 
 ?>
+<script>
 
+$(document).ready(function(){
+	$("#toggle-login").click(function(){
+		$(this).parent().hide();
+		$(".register-form").show();	
+	});
+	$("#toggle-register").click(function(){
+		$(this).parent().hide();	
+		$(".login-form").show();
+	});
+	
+});
+
+</script>
 </body>
 </html>
---
+++ postlandr.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Simple example of extending the SQLite3 class and changing the __construct
+ * parameters, then using the open method to initialize the DB.
+ */
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('mysqlitedb.db');
+    }
+}
+
+$db = new MyDB();
+
+$db->exec('
+	CREATE TABLE if not exists users (
+	id INT PRIMARY KEY NOT NULL,
+ 	email TEXT NOT NULL,
+ 	name TEXT NOT NULL,
+ 	password CHAR(32),
+ 	todayTime TEXT NOT NULL,
+ 	todayDate TEXT NOT NULL,
+ 	lastTimeTime TEXT NOT NULL
+ 	)');
+//$db->exec("INSERT INTO foo (bar) VALUES ('This is a test')");
+
+//$result = $db->query('SELECT bar FROM foo');
+//var_dump($result->fetchArray());
+
+if (isset($_POST['name']))
+	$name = $_POST['name'];
+if (isset($_POST['passone']))	
+$passone = md5($_POST['passone']);
+if (isset($_POST['passtwo']))
+	$passtwo = md5($_POST['passtwo']);
+if (isset($_POST['email']))		
+	$email = $_POST['email'];
+
+$id = rand(11111,99999);
+//$result = $db->exec("SELECT id FROM users WHERE id = '$id'");
+$result = $db->query("SELECT COUNT(id) as count FROM users WHERE id = '$id'");
+$row = $result->fetchArray();
+$numRows = $row['count'];
+echo $numRows;
+
+
+if(isset($_POST['login'])){
+	//$result = $db->exec("INSERT INTO users VALUES ('This is a test')");
+	//var_dump($result->fetchArray());
+}else
+	$_POST['login'] = "";
+
+
+$db->close();
+?>
